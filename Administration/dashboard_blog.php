d7ce0d00df1d4ba3da13094d43420c7d35c4592b
ClementDurand81 <clement.durand.cci@gmail.com>
2024-05-20 17:43:33 +0200

    Blog V1

--- Administration/dashboard_blog.php
+++ Administration/dashboard_blog.php
@@ -1,8 +1,9 @@
 <?php
-// Initialisation de la session
-session_start();
+// Inclure la connexion à la base de données
+include "../Serveur/bdd.php";
 
 // Vérifier si l'utilisateur est connecté
+session_start();
 if (!isset($_SESSION['user_id'])) {
     // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
     header("Location: ../login.php");
@@ -15,17 +16,30 @@ if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
     header("Location: ../index.php");
     exit;
 }
+
+// Récupérer toutes les machines de la base de données
+$sql = "SELECT * FROM blogs";
+$stmt = $bdd->query($sql);
+if ($stmt === false) {
+    // Erreur lors de l'exécution de la requête SQL
+    http_response_code(500);
+    exit("Erreur lors de la récupération des données des blogs.");
+}
+$machines = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
+    <title>Blogs</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/dashboard.css">
 </head>
+
 <body>
     <div class="sidebar">
         <h2>Menu</h2>
@@ -40,3 +54,134 @@ if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
             <a href="../Serveur/deconnexion.php">Se déconnecter</a>
         </div>
     </div>
+
+    <div class="content">
+        <div class="header">
+            <h2>Blogs</h2>
+        </div>
+        <div class="options-container">
+            <label>
+                <input type="radio" name="userOption" value="afficher" onclick="afficherFormulaire()">
+                <h3>Ajouter</h3>
+            </label>
+            <label>
+                <input type="radio" name="userOption" value="accepter" onclick="afficherTableauModifier()">
+                <h3>Modifier</h3>
+            </label>
+            <label>
+                <input type="radio" name="userOption" value="supprimer" onclick="afficherTableauSupprimer()">
+                <h3>Supprimer</h3>
+            </label>
+        </div>
+        <br>
+        <div id="formulaire" class="hidden">
+            <form action="../Serveur/ajouter_blog.php" method="POST" enctype="multipart/form-data">
+                <div>
+                    <label for="nom">Titre du blog :</label>
+                    <input type="text" id="nom" name="nom" required>
+                </div>
+                <div>
+                    <label for="image1">Image du blog :</label>
+                    <input type="file" id="image1" name="image1" accept="image/*" required>
+                </div>
+                <div>
+                    <label for="petiteDescription">Petite Description :</label>
+                    <textarea id="petiteDescription" name="petiteDescription" rows="4" required></textarea>
+                </div>
+                <div>
+                    <label for="image2">Image 1 :</label>
+                    <input type="file" id="image2" name="image2" accept="image/*" required>
+                </div>
+                <div>
+                    <label for="description1">Description 1:</label>
+                    <textarea id="description1" name="description1" rows="4" required></textarea>
+                </div>
+                <div>
+                    <label for="image3">Image 2 :</label>
+                    <input type="file" id="image3" name="image3" accept="image/*" required>
+                </div>
+                <div>
+                    <label for="description2">Description 2:</label>
+                    <textarea id="description2" name="description2" rows="4" required></textarea>
+                </div>
+                <button type="submit">Ajouter</button>
+            </form>
+        </div>
+
+        <!-- Structure HTML pour le tableau -->
+        <div id="tableau" class="hidden">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Titre</th>
+                        <th>Image blog</th>
+                        <th>Petite Description</th>
+                        <th>Image 1</th>
+                        <th>Description 1</th>
+                        <th>Image 2</th>
+                        <th>Description 2</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($blogs as $blog) : ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($blog['id_blog']); ?></td>
+                            <td><?php echo htmlspecialchars($blog['Titre']); ?></td>
+                            <td><img src="../<?php echo htmlspecialchars($blog['Image_1']); ?>" alt="<?php echo htmlspecialchars($blog['Titre']); ?>" style="width: 100px;"></td>
+                            <td><?php echo htmlspecialchars($blog['petiteDescription']); ?></td>
+                            <td><img src="../<?php echo htmlspecialchars($blog['Image_2']); ?>" alt="<?php echo htmlspecialchars($blog['Titre']); ?>" style="width: 100px;"></td>
+                            <td><?php echo htmlspecialchars($blog['Description_1']); ?></td>
+                            <td><img src="../<?php echo htmlspecialchars($blog['Image_3']); ?>" alt="<?php echo htmlspecialchars($blog['Titre']); ?>" style="width: 100px;"></td>
+                            <td><?php echo htmlspecialchars($blog['Description_2']); ?></td>
+                            <td><button class="btn btn-modifier" onclick="afficherFormulaireModifier('<?php echo htmlspecialchars($blog['id_blog']); ?>', '<?php echo htmlspecialchars($blog['Titre']); ?>', '<?php echo htmlspecialchars($machine['Image_1']); ?>', '<?php echo htmlspecialchars($blog['petiteDescription']); ?>', '<?php echo htmlspecialchars($machine['Image_2']); ?>', '<?php echo htmlspecialchars($machine['Description_1']); ?>' , '<?php echo htmlspecialchars($machine['Image_3']); ?>', '<?php echo htmlspecialchars($machine['Description_2']); ?>')">Modifier</button></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Structure HTML pour la carte pop-up -->
+        <div id="popupForm" class="popup hidden">
+            <div class="popup-content">
+                <div id="formulaireModifier" class="hidden">
+                    <!-- Le formulaire sera injecté ici -->
+                </div>
+            </div>
+        </div>
+
+        <div id="overlay" class="overlay hidden"></div>
+
+
+        <div id="tableau2" class="hidden">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nom</th>
+                        <th>Image</th>
+                        <th>Petite Description</th>
+                        <th>Description</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($machines as $machine) : ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($machine['id_machines']); ?></td>
+                            <td><?php echo htmlspecialchars($machine['Titre']); ?></td>
+                            <td><img src="../<?php echo htmlspecialchars($machine['Image']); ?>" alt="<?php echo htmlspecialchars($machine['Titre']); ?>" style="width: 100px;"></td>
+                            <td><?php echo htmlspecialchars($machine['petiteDescription']); ?></td>
+                            <td><?php echo htmlspecialchars($machine['Description']); ?></td>
+                            <td><button onclick="supprimerMachine(<?php echo htmlspecialchars($machine['id_machines']); ?>)">Supprimer</button></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="../assets/js/dashboard_blogs.js"></script>
+</body>
+
+</html>
---
+++ Serveur/ajouter_blog.php
@@ -0,0 +1,87 @@
+<?php
+// Vérifie si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Inclure la connexion à la base de données
+    require_once "bdd.php";
+
+    // Fonction pour nettoyer les données
+    function cleanInput($data)
+    {
+        // Supprimer les balises HTML et PHP et appliquer l'échappement de caractères spéciaux
+        return htmlspecialchars(strip_tags(trim($data)));
+    }
+
+    // Vérifier si les champs sont vides
+    if (!empty($_POST['nom']) && !empty($_FILES['image1']['name']) && !empty($_POST['petiteDescription']) && !empty($_POST['description1']) && !empty($_FILES['image2']['name'])  && !empty($_POST['description2']) && !empty($_FILES['image3']['name'])) {
+        // Récupérer et nettoyer les données du formulaire
+        $nom = cleanInput($_POST['nom']);
+        $petiteDescription = cleanInput($_POST['petiteDescription']);
+        $description1 = cleanInput($_POST['description1']);
+        $description2 = cleanInput($_POST['description2']);
+
+        // Tableau pour stocker les destinations des images
+        $image_destinations = [];
+
+        // Traitement des images
+        for ($i = 1; $i <= 3; $i++) {
+            $image = $_FILES['image' . $i];
+            $image_name = cleanInput($image['name']);
+            $image_tmp = $image['tmp_name'];
+            $image_size = $image['size'];
+            $image_error = $image['error'];
+
+            // Obtenir l'extension de l'image
+            $image_extension = strtolower(pathinfo($image_name, PATHINFO_EXTENSION));
+
+            // Tableau des extensions autorisées
+            $allowed_extensions = array('jpg', 'jpeg', 'png', 'gif');
+
+            // Vérifier si l'extension est autorisée
+            if (in_array($image_extension, $allowed_extensions)) {
+                // Chemin de destination de l'image
+                $image_destination = '../assets/img/imgBlogs/' . $image_name;
+
+                // Déplacer l'image téléchargée vers le dossier d'uploads
+                if (move_uploaded_file($image_tmp, $image_destination)) {
+                    $image_destinations[] = $image_destination;
+                } else {
+                    echo "Erreur lors du téléchargement de l'image " . $i . ".";
+                    exit;
+                }
+            } else {
+                echo "Erreur : Format d'image non pris en charge pour l'image " . $i . ". Veuillez télécharger une image au format jpg, jpeg, png ou gif.";
+                exit;
+            }
+        }
+
+        try {
+            // Préparation de la requête SQL avec des paramètres nommés
+            $sql = "INSERT INTO blogs (Titre, Image_1, Image_2, Image_3, petiteDescription, Description_1, Description_2) VALUES (:nom, :image1, :image2, :image3, :petiteDescription, :description1, :description2)";
+            $stmt = $bdd->prepare($sql);
+
+            // Liaison des valeurs aux paramètres nommés
+            $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':image1', $image_destinations[0]);
+            $stmt->bindParam(':petiteDescription', $petiteDescription);
+            $stmt->bindParam(':description1', $description1);
+            $stmt->bindParam(':image2', $image_destinations[1]);
+            $stmt->bindParam(':description2', $description2);
+            $stmt->bindParam(':image3', $image_destinations[2]);
+
+            // Exécution de la requête
+            if ($stmt->execute()) {
+                // Si l'ajout est réussi, rediriger vers dashboard_machine.php avec un paramètre de succès
+                header("Location: ../Administration/dashboard_blog.php?success=true");
+                exit;
+            } else {
+                echo "Erreur lors de l'exécution de la requête.";
+            }
+        } catch (PDOException $e) {
+            // En cas d'erreur lors de l'exécution de la requête, affichage de l'erreur
+            echo "Erreur : " . $e->getMessage();
+        }
+    } else {
+        echo "Erreur : Veuillez remplir tous les champs.";
+    }
+}
+?>
